2e6ebadd976aa5a69ca71b0ac830fc4137fef131
André Lacerda <andrlacerda180@gmail.com>
2021-12-27 13:30:36 -0300

    create Card component for remove duplicated code

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,12 +1,13 @@
+import { Card } from './Card'
 import { ExpenseItem } from './ExpenseItem'
 import './Expenses.css'
 
 export function Expenses({ items }) {
   return (
-    <div className='expenses'>
+    <Card className='expenses'>
       <ExpenseItem title={items[0].title} amount={items[0].amount} date={items[0].date} />
       <ExpenseItem title={items[1].title} amount={items[1].amount} date={items[1].date} />
       <ExpenseItem title={items[2].title} amount={items[2].amount} date={items[2].date} />
-    </div>
+    </Card>
   )
 }
